1cb7ffd10ad2a008bfcfc06dc0acb333449c50b3
celesq <raresutuboss@gmail.com>
2025-05-11 17:43:39 +0300

    Added files

---
+++ client.c
@@ -0,0 +1,13 @@
+#include <stdio.h>      /* printf, sprintf */
+#include <stdlib.h>     /* exit, atoi, malloc, free */
+#include <unistd.h>     /* read, write, close */
+#include <string.h>     /* memcpy, memset */
+#include <sys/socket.h> /* socket, connect */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#include <netdb.h>      /* struct hostent, gethostbyname */
+#include <arpa/inet.h>
+#include "helpers.h"
+#include "requests.h"
+
+#define HOST "54.217.160.10"
+#define PORT 8080
